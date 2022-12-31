99e48927cdb1bd5b044062d080b71c597001ce66
Nitin <nitinshankarmadhu@gmail.com>
2022-12-31 00:34:21 -0500

    linked list in cpp

---
+++ linked_list/listclass.hpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+class Node 
+{
+ 
+    public:
+    int data;
+    Node* next;
+    Node(int data, Node* ptr = nullptr){
+        this->data = data;
+        this->next = ptr;
+    }
+
+    int return_data(){
+        return this->data;
+    }
+    Node* return_next(){
+        return next;
+    }
+    void set_next(Node* node){
+        next = node;
+    }
+};
+
+class LL 
+{
+    public:
+    LL()
+    {
+        this->head = nullptr;  
+    }
+
+    void addToStart (int data)
+    {
+        if (head != nullptr)
+        {   
+            Node * temp = new Node(data, head);
+            head = temp;
+        }
+        else
+        {
+            Node * temp = new Node(data, nullptr);
+            head = temp;
+        }
+    }
+
+    void addToEnd(int data)
+    {
+        Node* temp = head;
+        while (temp->return_next() != nullptr)
+        {
+            temp = temp->return_next();
+        }
+        Node* element = new Node(data, nullptr);
+        temp->next = element;
+    }
+
+    void printAll()
+    {
+        Node * temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->return_data()<<endl;
+            temp = temp->return_next();
+        }
+    }
+    Node* head;
+}; 
+
+
